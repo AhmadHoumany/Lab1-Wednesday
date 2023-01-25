1ce576c1151a82d60c50e2da50958eede31706a8
Mehanna <serge.mehanna@eng.lau.edu.lb>
2023-01-25 20:34:35 +0200

    Face is done by Serge Mehanna

--- Face.java
+++ Face.java
@@ -1,7 +1,7 @@
 public class Face {
     public static void main(String[] args)
     {
-        System.out.println(" +\"\"\"\"\"+ ");
+        System.out.println(" + \"\"\"\"\" +");
         System.out.println("[| o o |]");
         System.out.println(" |  ^  | ");
 	System.out.println(" | '-' | ");
